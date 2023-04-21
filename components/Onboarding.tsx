f34a594f688a82d8cba2c65cc74b948e8beb62f0
Sam Biggins <sambiggins@gmail.com>
2023-04-20 19:51:22 -0700

    style: add theming to onboarding

--- components/Onboarding.tsx
+++ components/Onboarding.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 
