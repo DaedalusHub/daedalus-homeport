a779e94be870c897554fafb0c0bfdad64aea0c63
Sam Biggins <sambiggins@gmail.com>
2023-04-23 11:27:48 -0700

    Project Prompt Generation (#6)
    * chore: reduce daedalus image size
    
    * chore: remove .idea files
    
    * feat: add project onboarding
    
    * chore: update packages, bump minor
    
    * v0.3.0
    
    * build: remove auto-versioning
    
    * style: add theming to onboarding
    
    * style: add theming to onboarding
    
    * feat(onboarding): add theming, redis backend
    
    * chore: add logs to .gitignore
    
    * fix(onboarding): fix redis set function
    
    * refactor(onboarding): break component in to multiple files for easier GPT processing
    
    * feat(onboarding): fetch existing projects, populate data from selected project
    
    * style(onboarding): beautify layout, move all items to single page
    
    * docs: add PROMPTS.md for documenting ChatGPT seeding prompt
    
    * refactor: remove extra file comments, no longer needed due to PROMPTS.md usage

---
+++ lib/logger.ts
@@ -0,0 +1,42 @@
+import log, { setLevel } from "loglevel";
+import * as process from "process";
+let winston;
+
+if (typeof window === "undefined") {
+    winston = require("winston");
+}
+if (typeof window !== "undefined") {
+    const logLevel = process.env.LOG_LEVEL || "info"
+    setLevel(logLevel as log.LogLevelDesc);
+}
+
+
+export const getLogger = (name: string) => {
+    return winston
+        ? winston.createLogger({
+            level: "info",
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.colorize(),
+                        winston.format.printf(
+                            (info) =>
+                                `${info.timestamp} ${info.level}: ${info.message}`
+                        )
+                    ),
+                }),
+                new winston.transports.File({
+                    filename: "logs/server.log",
+                    level: process.env.LOG_LEVEL || "info",
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.printf(
+                            (info) => `${info.timestamp} ${info.level}: ${info.message}`
+                        )
+                    ),
+                }),
+            ],
+        }).child({ label: name })
+        : log.getLogger(name);
+};
---
+++ pages/api/get-projects.ts
@@ -0,0 +1,23 @@
+// pages/api/get-projects.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getAllProjects } from '@/lib/project';
+import { getLogger } from '@/lib/logger';
+
+const log = getLogger('API');
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        log.info(`Fetching all projects`);
+
+        try {
+            const projects = await getAllProjects();
+            res.status(200).json(projects);
+        } catch (error) {
+            log.error(`Error fetching projects: ${error}`);
+            res.status(500).json({ message: error.message });
+        }
+    } else {
+        res.status(405).json({ message: 'Method not allowed' });
+    }
+};
